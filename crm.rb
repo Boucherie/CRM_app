8f4e80ef6d785b901af95886064b76302167057d
Emily Boucher <emi.boucher@gmail.com>
2017-06-05 15:09:31 -0400

    add contact page working, increased p font size, added erb edit contact page with route (untested)

--- crm.rb
+++ crm.rb
@@ -14,6 +14,16 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/contacts/new' do
+  erb :new_contact
+end
+
+get 'contacts/:id/edit' do
+  erb :edit_contact
+end
+
+
+
 get '/contacts/:id' do
   @contact = Contact.find_by(params[:id].to_i)
   if @contact
@@ -27,8 +37,14 @@ get '/about' do
   erb :about
 end
 
-get '/new' do
-  erb :new_contact
+post '/contacts' do
+  Contact.create(
+  first_name: params[:first_name],
+  last_name:  params[:last_name],
+  email:      params[:email],
+  note:       params[:note]
+  )
+  redirect to('/contacts')
 end
 
 after do
