1cc2930e1cbe29e619f1e7873df68bf953f4d3db
Emily Boucher <emi.boucher@gmail.com>
2017-06-05 17:49:21 -0400

    edit and delete functions working

--- crm.rb
+++ crm.rb
@@ -1,5 +1,6 @@
 require_relative 'contact'
 require 'sinatra'
+require 'pry'
 
 get '/' do #root
 @contact_list = Contact.all
@@ -7,6 +8,7 @@ get '/' do #root
 end
 
 get '/contacts' do
+
   @contact_list = Contact.all
 
   @link = "https://www.facebook.com"
@@ -18,14 +20,19 @@ get '/contacts/new' do
   erb :new_contact
 end
 
-get 'contacts/:id/edit' do
-  erb :edit_contact
+get '/contacts/:id/edit' do
+  # raise @contact.inspect
+  #binding.pry
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
-
-
 get '/contacts/:id' do
-  @contact = Contact.find_by(params[:id].to_i)
+  @contact = Contact.find_by(id: params[:id].to_i)
   if @contact
     erb :show_contact
   else
@@ -45,6 +52,30 @@ post '/contacts' do
   note:       params[:note]
   )
   redirect to('/contacts')
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 after do
